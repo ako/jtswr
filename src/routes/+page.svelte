6fba6cfefa448851d8ea6cfc5250f6e840baf2e5
Andrej Koelewijn <andrej@koelewijn.net>
2023-02-21 22:20:32 +0100

    Dev

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
+<script lang="ts">
+    import { AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<AppBar>Jumpteam Wing Race</AppBar>
+<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+	<svelte:fragment slot="lead">(icon)</svelte:fragment>
+	(title)
+	<svelte:fragment slot="trail">(actions)</svelte:fragment>
+</AppBar>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,22 @@
+import preprocess from "svelte-preprocess";
 /*import adapter from '@sveltejs/adapter-auto';*/
 import cloudflare from "@sveltejs/adapter-cloudflare";
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from "@sveltejs/kit/vite";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
+  // for more information about preprocessors
+  preprocess: [
+    vitePreprocess(),
+    preprocess({
+      postcss: true,
+    }),
+  ],
 
-	kit: {
-		adapter: cloudflare()
-	}
+  kit: {
+    adapter: cloudflare(),
+  },
 };
 
 export default config;
